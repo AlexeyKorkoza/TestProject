99f76f3c9a2865a437cd4daf7132433b55d423fd
AlexeyKorkoza <alexeykorkoza@gmail.com>
2016-12-20 20:51:46 +0300

    Removing Hashbang from URL, change size of icon

--- js/route.js
+++ js/route.js
@@ -1,11 +1,10 @@
 'use strict';
 
 var myApp = angular.module('myApp');
-myApp.config(function ($routeProvider) {
+myApp.config(function ($routeProvider, $locationProvider) {
   $routeProvider
     .when('/', {
       templateUrl: '../../templates/main.html',
       controller: 'mainCtrl'
     })
-    .otherwise({redirectTo: '/'});
 });
